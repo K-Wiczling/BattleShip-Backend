cda47e79bf6d3a005ad7e90c39286a0047bf3b25
K-Wiczling <wiczling.karol@gmail.com>
2023-02-26 18:04:54 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+
+app.listen(3001, () => {
+    console.log("App is runninig on port 3001" );
+});
