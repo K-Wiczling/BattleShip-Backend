50c821c10acd3d1c0816b73ea2782ec1d8e68370
kwiczling <wiczling.karol@gmail.com>
2023-03-22 21:12:21 +0100

    continue working on the registratioj endpoint

--- src/registration/register.js
+++ src/registration/register.js
@@ -1,4 +1,4 @@
-// const bcrypt = require("bcrypt");
+import bcrypt from 'bcrypt'
 
 export default class Register {
     constructor (email, name, password) {
@@ -8,7 +8,15 @@ export default class Register {
 
         this.email = email;
         this.name = name;
-        this.password = password;
+        this.saltRounds = 10
+        this.hash;
+
+        bcrypt.hash(password, this.saltRounds).then(function(hash) {
+            // Store hash in your password DB.
+            // this.hash = hash;
+            console.log(hash);
+        });
+
 
         //Singletone
         Register.exists = true;
